296be2ab4a8264940a498022814519579438e301
loknarb <brumbaughco@gmail.com>
2023-01-29 14:57:30 +0100

    imported jwt and supabase adapters

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { SupabaseAdapter } from "@next-auth/supabase-adapter";
+import jwt from "jsonwebtoken";
 
 // eslint-disable-next-line import/no-default-export
 export default NextAuth({
@@ -25,4 +27,30 @@ export default NextAuth({
     //   from: 'NextAuth.js <no-reply@example.com>'
     // }),
   ],
+
+  // For more information on each option (and a full list of options) go to
+  // https://next-auth.js.org/configuration/options
+  // https://next-auth.js.org/configuration/providers
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  }),
+  callbacks: {
+    async session({ session, user }) {
+      const signingSecret = process.env.SUPABASE_JWT_SECRET;
+      if (signingSecret) {
+        const payload = {
+          aud: "authenticated",
+          exp: Math.floor(new Date(session.expires).getTime() / 1000),
+          sub: user.id,
+          email: user.email,
+          role: "authenticated",
+        };
+        // eslint-disable-next-line no-param-reassign
+        session.supabaseAccessToken = jwt.sign(payload, signingSecret);
+      }
+      return session;
+    },
+  },
+  // ...
 });
