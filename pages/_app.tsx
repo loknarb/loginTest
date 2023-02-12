71fd33979a21b7f9fe94553849002972a7238ebf
loknarb <brumbaughco@gmail.com>
2023-02-12 13:49:39 +0100

    added session object pass through into app for useSession sue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
 export function App({ Component, pageProps }: AppProps) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={pageProps.session}>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
