f18e4ed5973fc8ce5ab08dcfc082234a89f14cde
loknarb <brumbaughco@gmail.com>
2023-02-12 13:35:26 +0100

    added supabase createclient function with param pass through

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,22 @@
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "./db.types";
+
+export const supabaseAuth = (supabaseAccessToken?: string) => {
+  if (supabaseAccessToken) {
+    const supabase = createClient<Database>(
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      process.env.SUPABASE_URL!,
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      process.env.SUPABASE_ANON_KEY!,
+      {
+        global: {
+          headers: {
+            Authorization: `Bearer ${supabaseAccessToken}`,
+          },
+        },
+      }
+    );
+    return supabase;
+  }
+  throw new Error("No supabase access token");
+};
