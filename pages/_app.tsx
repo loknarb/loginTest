5ac28369c2bcbab3bfb3046d9305d5969e5d6e06
loknarb <brumbaughco@gmail.com>
2023-02-15 19:43:31 +0100

    destructed session in param

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 
-export function App({ Component, pageProps }: AppProps) {
+export function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={session}>
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
     </SessionProvider>
