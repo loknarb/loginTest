65e24e4acc3e58b76bd5c7f1199a98cbbce921f3
loknarb <brumbaughco@gmail.com>
2023-02-21 17:06:48 +0100

    added polymorphic button functionality

--- components/main/Button/index.tsx
+++ components/main/Button/index.tsx
@@ -1,13 +1,26 @@
-import React from "react";
-
-interface ButtonProps {
-  name?: string;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  icon?: React.ReactNode;
+  // color?: "default" | "primary";
+  loading?: boolean;
 }
 
-const Button = ({ name }: ButtonProps) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  // color = "default",
+  icon,
+  loading = false,
+  ...props
+}) => {
   return (
-    <button className="btn btn-ghost normal-case text-xl" type="button">
-      {name || "default props"}
+    <button
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+      type="button"
+      className="w-full px-4 py-1 bg-slate-500"
+      disabled={loading}>
+      {icon}
+      {children}
     </button>
   );
 };
