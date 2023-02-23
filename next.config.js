5b4dc28d40814e75b39fabf4dc960667e5baedb2
loknarb <brumbaughco@gmail.com>
2023-02-23 20:18:51 +0100

    implemented next-i18next translations

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { i18n } = require("./next-i18next.config");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
